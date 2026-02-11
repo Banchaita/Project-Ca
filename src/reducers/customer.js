9b275bdb995ded2a38dca5a030e52dbb3508706a
Banchaita <bisassanchaita1998@gmail.com>
2026-02-11 12:58:47 +0530

    chnage redux

--- src/reducers/customer.js
+++ src/reducers/customer.js
@@ -1,24 +1,22 @@
 import { BOOK_SLOTS } from "../actions/type";
 
-const initialState= {
-    book_slot: []
-}
+const initialState = {
+  book_slot: []
+};
 
-export default (state = initialState, action) => {
-    const { type, payload } = action;
+const customerReducer = (state = initialState, action) => {
+  const { type, payload } = action;
 
-    switch (type){
-        case BOOK_SLOTS:
-            console.log("payload",payload)
-        return {
-           ...state,
-            book_slot: payload
-        }
-        default:{
-            return state;
-        }
-    }
-}
+  switch (type) {
+    case BOOK_SLOTS:
+      return {
+        ...state,
+        book_slot: payload
+      };
 
+    default:
+      return state;
+  }
+};
 
-
+export default customerReducer;
