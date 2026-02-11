eda30e4816ce402155578325267e8bca0d6543eb
Banchaita <bisassanchaita1998@gmail.com>
2026-02-11 14:18:55 +0530

    Disable ESLint during build

--- src/components/Loader/OtherPageLoader.js
+++ src/components/Loader/OtherPageLoader.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHandsPraying } from "@fortawesome/free-regular-svg-icons";
 
 const OtherPageLoader = () => {
   return (
